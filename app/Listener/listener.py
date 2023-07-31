eb95ae4462aa18a82e0db97f1fbb24cdcad5dd87
Narendra.Singodia <narendra.singodia@FVFJ61YNQ6L4.local>
2023-07-31 11:55:32 +0530

    Added file reading and writing in listeners

---
+++ app/Listener/listener.py
@@ -0,0 +1,22 @@
+import aiofiles
+from app.manager.translator import Google,Rapid,Lacto
+
+async def read_file():
+    async with aiofiles.open('performance_log.txt', mode='r') as f:
+        contents = await f.read()
+        contents = contents.split('/n')
+        Google.success,Google.failure = eval(contents[0])
+        Rapid.success,Rapid.failure = eval(contents[1])
+        Lacto.success,Lacto.failure = eval(contents[2])
+        print(Google.success,Google.failure)
+        print(Rapid.success,Rapid.failure)
+        print(Lacto.success,Lacto.failure)
+
+async def write_file():
+    async with aiofiles.open('performance_log.txt', mode='w') as f:
+        await f.write(str((Google.success,Google.failure))+'/n')
+        await f.write(str((Rapid.success,Rapid.failure))+'/n')
+        await f.write(str((Lacto.success,Lacto.failure))+'/n')
+        print(Google.success,Google.failure)
+        print(Rapid.success,Rapid.failure)
+        print(Lacto.success,Lacto.failure)
--- app/serivce.py
+++ app/serivce.py
@@ -1,5 +1,5 @@
 from sanic import Sanic
-
+from app.Listener.listener import read_file,write_file
 from app.routes.detect_routes import detect_bp
 from app.routes.translate_routes import trans_bp
 
@@ -9,11 +9,14 @@ app = Sanic('Translator')
 app.blueprint(detect_bp)
 app.blueprint(trans_bp)
 
-
 @app.before_server_start
-async def readthefile():
-    
-    pass
+async def read_data(app,_):
+    await read_file()
+
+@app.after_server_stop
+async def write_data(main: Sanic, _):
+    await write_file()
+
 
 if __name__ == '__main__':
     app.run(auto_reload=True)
